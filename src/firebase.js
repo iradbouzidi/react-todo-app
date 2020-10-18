c6bb9fb80091a9261ec883ec6621fc2f53434034
iradbouzidi <irad.bouzidi@esprit.tn>
2020-10-18 02:09:56 +0100

    last touches

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,14 @@
 import firebase from "firebase";
 
 const firebaseApp = firebase.initializeApp({
-    apiKey: "AIzaSyDUis3zUK_HESQN0fgk1haBnv4PfxZO8xo",
-    authDomain: "todo-app-irad.firebaseapp.com",
-    databaseURL: "https://todo-app-irad.firebaseio.com",
-    projectId: "todo-app-irad",
-    storageBucket: "todo-app-irad.appspot.com",
-    messagingSenderId: "182512480774",
-    appId: "1:182512480774:web:632273a09f9d74a5a64987",
-    measurementId: "G-89FE99C1Y3"
+  apiKey: "AIzaSyAYaaHDm2-4_VtjHvdniEbQ0h4_NhJ3k30",
+  authDomain: "todo-app-irad-51a26.firebaseapp.com",
+  databaseURL: "https://todo-app-irad-51a26.firebaseio.com",
+  projectId: "todo-app-irad-51a26",
+  storageBucket: "todo-app-irad-51a26.appspot.com",
+  messagingSenderId: "297689276271",
+  appId: "1:297689276271:web:5e1c86ce8bf8255bb3529a",
+  measurementId: "G-40GJ2G3G2Y",
 });
 
 const db = firebaseApp.firestore();
